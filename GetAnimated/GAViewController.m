1f8b406bd44a278559adc0c91c8c50d901a8ae91
sarah <lingwall76@gmail.com>
2015-06-02 11:42:17 -0600

    Commit

--- GetAnimated/GAViewController.m
+++ GetAnimated/GAViewController.m
@@ -10,7 +10,8 @@
 
 @interface GAViewController ()
 
-@property (nonatomic, strong) UIView *block;
+@property (nonatomic, strong) UIView *square;
+@property (nonatomic, strong) UIView *smallSquare;
 
 @end
 
@@ -20,11 +21,43 @@
 {
     [super viewDidLoad];
     
-    UIView *block = [[UIView alloc] initWithFrame:CGRectMake(60, 60, 200, 200)];
-    block.backgroundColor = [UIColor cyanColor];
-    [self.view addSubview:block];
-    self.block = block;
+    UIView *square = [[UIView alloc] initWithFrame:CGRectMake(60, 60, 200, 200)];
+    square.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:square];
+    self.square = square;
     
+    UIView *smallSquare = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 120, 120)];
+    smallSquare.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:smallSquare];
+    self.smallSquare = smallSquare;
+//
+//    CGAffineTransform scaleTransform = CGAffineTransformMakeScale(2.0, 2.0);
+//    CGAffineTransform rotateTransform = CGAffineTransformMakeRotation(90 * M_PI / 180);
+//    CGAffineTransform halfScaleTransform = CGAffineTransformMakeScale(0.5, 0.5);
+//    
+//    [UIView animateWithDuration:3.0 animations:^{
+//        self.square.alpha = 0.0;
+//        self.square.center = CGPointMake(self.square.center.x, self.square.center.y + 300);
+//        self.square.transform = CGAffineTransformConcat(scaleTransform, rotateTransform);
+//        self.smallSquare.transform = CGAffineTransformConcat(scaleTransform, rotateTransform);
+//        self.smallSquare.alpha = 0.0;
+//        self.smallSquare.center = CGPointMake(self.smallSquare.center.x, self.smallSquare.center.y - 100);
+//
+//
+//}];
+//
+    
+
+
+//    CABasicAnimation *animation = [CABasicAnimation animation];
+//    animation.keyPath = @"position.y";
+//    animation.fromValue = @(self.square.center.y);
+//    animation.toValue = @(self.square.center.y + 300);
+//    animation.duration = 3;
+//    
+//    [self.square.layer addAnimation:animation forKey:@"basic"];
+//    
+//    self.square.layer.position = CGPointMake(self.square.layer.position.x, self.square.layer.position.y + 300);
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,5 +65,20 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
